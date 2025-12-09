0a68ec47cb6a8ca1390f5a704eafd72e69bcaf06
lucas2006-V <lucas.vlaeminck@ugent.be>
2025-12-09 10:41:56 +0100

    gekopieerd

--- components/specificComponents/contact/contact.js
+++ components/specificComponents/contact/contact.js
@@ -1,23 +1,52 @@
 import React, { Component } from "react";
+import css from "./contact.module.scss";
 import Headermenu from "../../genericComponents/Headermenu/Headermenu";
 import Hero from "../../genericComponents/Hero/Hero";
-import TeacherCard from "../TeacherCard/TeacherCard";
-import Element from "../../genericComponents/Element/Element";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
-export default class contact extends Component {
+export default class Contact extends Component {
+  render() {
+    const blok = this.props.blok;
 
-	constructor(props) {
-		super(props);
-	}
+    return (
+      <div {...storyblokEditable(blok)} className={css["contact-page"]}>
+        
+        {/* Menu en logo */}
+        <Headermenu blok={this.props.menu.content} />
 
-	render() {
-		return (
-			<div {...storyblokEditable(this.props.blok)}>
-                testen 
-		    </div>
-		);
+        <main>
+          {/* Hero banner */}
+          <Hero blok={this.props.blok} contentTypeTag="contact" />
 
-	}
+          {/* Tekst links, foto rechts */}
+          <div className={css["contact-page__main-content"]}>
+            
+            {/* Tekst */}
+            <div className={css["contact-page__text"]}>
+              {blok.name && <h1>{blok.name}</h1>}
+              {blok.experience && (
+                <p>{blok.experience}</p>
+              )}
+              {blok.email && <p><strong>Email:</strong> {blok.email}</p>}
+              {blok.phone_number && <p><strong>Phone:</strong> {blok.phone_number}</p>}
+            </div>
+
+            {/* Foto */}
+            {blok.image && (
+              <div className={css["contact-page__image"]}>
+                <img src={blok.image.filename} alt={blok.name} />
+              </div>
+            )}
+          </div>
+
+          {/* Bottomblocks / Justawk integratie */}
+          {blok.bottomblocks && blok.bottomblocks.map((nestedBlok) => (
+            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+          ))}
+        </main>
+
+      </div>
+    );
+  }
 }
