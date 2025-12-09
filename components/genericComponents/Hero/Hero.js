b2c2310bce3474175cd6db3f9ed6778c17ce3f0a
Sandervlb <Sandervanlerberghe@hotmail.com>
2025-12-09 14:58:14 +0100

    hero

--- components/genericComponents/Hero/Hero.js
+++ components/genericComponents/Hero/Hero.js
@@ -8,36 +8,70 @@ import "lazysizes/plugins/attrchange/ls.attrchange";
 import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer"; 
 import { getDefaultStoryBlokImageSet } from "../../../functions/StoryBlokImageHelper";
 
-
 export default function Hero({ blok }) {
-	let showImage = (blok.image && blok.image.filename);
-	let colorCssName = "--" + blok.colorcode?.content.title;
-	let titleString = typeof blok.title === "string" ? blok.title : undefined;
-	let taglineString = typeof blok.tagline === "string" ? blok.tagline : undefined;
+
+	// VEILIGE checks
+	const showImage = blok?.image?.filename || null;
+
+	const colorCssName = blok?.colorcode?.content?.title
+		? "--" + blok.colorcode.content.title
+		: "--default"; // fallback class zodat CSS niet breekt
+
+	const titleString =
+		typeof blok.title === "string"
+			? blok.title
+			: blok.title
+			? RichTextToHTML({
+					document: blok.title,
+					textClassName: css["hero__title"],
+					boldClassName: css["hero__title--highlighted" + colorCssName],
+			  })
+			: "";
+
+	const taglineString =
+		typeof blok.tagline === "string"
+			? blok.tagline
+			: blok.Tagline
+			? RichTextToHTML({
+					document: blok.Tagline,
+					textClassName: css["hero__subtitle"],
+			  })
+			: "";
 
 	return (
-		<>
-			<div {...storyblokEditable(blok)} className={css["hero" + colorCssName]}>
-				<div className={[css["hero__container"], css["hero__container"]].join(" ")}>
-					<div className={[css["hero__title-group"], css["hero__title-group"]].join(" ")}>
-						<p className={css["hero__tag"]}>{blok.supertitle}</p>
-						<h1 className={css["hero__title"]}>{titleString || RichTextToHTML({ document: blok.title, textClassName: css["hero__title"], boldClassName: css["hero__title--highlighted" + colorCssName] })}</h1>
-						<div className={css["hero__subtitle"]}>{taglineString || RichTextToHTML({ document: blok.Tagline, textClassName: css["hero__subtitle"] })}</div>
-						{blok.tags && blok.tags.length > 0 && <TagList tags={blok.tags} variation={"white"} center />}
-					</div>
-					{showImage &&
-						<figure className={css["hero__image-responsive-wrapper"]}>
-							{getDefaultStoryBlokImageSet(blok.image.filename, blok.image.alt, { largestImageWidth: 870, largestImageHeigth: 870 }, 850, css["hero__image"])}
-						</figure>
-					}
+		<div {...storyblokEditable(blok)} className={css["hero" + colorCssName]}>
+			<div className={css["hero__container"]}>
 
+				<div className={css["hero__title-group"]}>
+					{blok.supertitle && (
+						<p className={css["hero__tag"]}>{blok.supertitle}</p>
+					)}
+
+					<h1 className={css["hero__title"]}>{titleString}</h1>
+
+					<div className={css["hero__subtitle"]}>{taglineString}</div>
+
+					{blok.tags && blok.tags.length > 0 && (
+						<TagList tags={blok.tags} variation="white" center />
+					)}
 				</div>
+
+				{showImage && (
+					<figure className={css["hero__image-responsive-wrapper"]}>
+						{getDefaultStoryBlokImageSet(
+							blok.image.filename,
+							blok.image.alt,
+							{ largestImageWidth: 870, largestImageHeigth: 870 },
+							850,
+							css["hero__image"]
+						)}
+					</figure>
+				)}
 			</div>
-		</>
-		
+		</div>
 	);
 }
 
 Hero.propTypes = {
-	blok: PropTypes.object
+	blok: PropTypes.object,
 };
--- components/specificComponents/contact/contact.js
+++ components/specificComponents/contact/contact.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import css from "./contact.module.scss";
 import Headermenu from "../../genericComponents/Headermenu/Headermenu";
-import Hero from "../../genericComponents/Hero/Hero";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
-import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
 export default class Contact extends Component {
   render() {
@@ -16,8 +14,7 @@ export default class Contact extends Component {
         <Headermenu blok={this.props.menu.content} />
 
         <main>
-          {/* Hero banner */}
-          <Hero blok={this.props.blok} contentTypeTag="contact" />
+          {/* Hero verwijderd */}
 
           {/* Tekst links, foto rechts */}
           <div className={css["contact-page__main-content"]}>
@@ -25,9 +22,7 @@ export default class Contact extends Component {
             {/* Tekst */}
             <div className={css["contact-page__text"]}>
               {blok.name && <h1>{blok.name}</h1>}
-              {blok.experience && (
-                <p>{blok.experience}</p>
-              )}
+              {blok.experience && <p>{blok.experience}</p>}
               {blok.email && <p><strong>Email:</strong> {blok.email}</p>}
               {blok.phone_number && <p><strong>Phone:</strong> {blok.phone_number}</p>}
             </div>
@@ -40,7 +35,7 @@ export default class Contact extends Component {
             )}
           </div>
 
-          {/* Bottomblocks / Justawk integratie */}
+          {/* Bottomblocks */}
           {blok.bottomblocks && blok.bottomblocks.map((nestedBlok) => (
             <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
           ))}
