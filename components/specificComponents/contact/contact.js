f7c74842df51293075555a7a9df6209d993a016a
Sandervlb <Sandervanlerberghe@hotmail.com>
2025-12-09 09:52:43 +0100

    contact.js

--- components/specificComponents/contact/contact.js
+++ components/specificComponents/contact/contact.js
@@ -1,35 +1,52 @@
 import React, { Component } from "react";
-import { storyblokEditable } from "@storyblok/react";
+import css from "./contact.module.scss";
+import Headermenu from "../../genericComponents/Headermenu/Headermenu";
+import Hero from "../../genericComponents/Hero/Hero";
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
+import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 
-export default class contact extends Component {
+export default class Contact extends Component {
+  render() {
+    const blok = this.props.blok;
 
-    render() {
-        const blok = this.props.blok;
+    return (
+      <div {...storyblokEditable(blok)} className={css["contact-page"]}>
+        
+        {/* Menu en logo */}
+        <Headermenu blok={this.props.menu.content} />
 
-        return (
-            <div {...storyblokEditable(blok)} className="contact-page">
+        <main>
+          {/* Hero banner */}
+          <Hero blok={this.props.blok} contentTypeTag="contact" />
 
-                {/* Naam of titel */}
-                {blok.name && (
-                    <h1>{blok.name}</h1>
-                )}
-
-                {/* Beschrijving (experience veld dat jij al hebt) */}
-                {blok.experience && (
-                    <p>{blok.experience}</p>
-                )}
-
-                {/* Email */}
-                {blok.email && (
-                    <p><strong>Email:</strong> {blok.email}</p>
-                )}
-
-                {/* Telefoon */}
-                {blok.phone_number && (
-                    <p><strong>Phone:</strong> {blok.phone_number}</p>
-                )}
-
+          {/* Tekst links, foto rechts */}
+          <div className={css["contact-page__main-content"]}>
+            
+            {/* Tekst */}
+            <div className={css["contact-page__text"]}>
+              {blok.name && <h1>{blok.name}</h1>}
+              {blok.experience && (
+                <p>{blok.experience}</p>
+              )}
+              {blok.email && <p><strong>Email:</strong> {blok.email}</p>}
+              {blok.phone_number && <p><strong>Phone:</strong> {blok.phone_number}</p>}
             </div>
-        );
-    }
+
+            {/* Foto */}
+            {blok.image && (
+              <div className={css["contact-page__image"]}>
+                <img src={blok.image.filename} alt={blok.name} />
+              </div>
+            )}
+          </div>
+
+          {/* Bottomblocks / Justawk integratie */}
+          {blok.bottomblocks && blok.bottomblocks.map((nestedBlok) => (
+            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+          ))}
+        </main>
+
+      </div>
+    );
+  }
 }
