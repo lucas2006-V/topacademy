4d25613341f21a418556153730f27bcb6108adda
lucas2006-V <lucas.vlaeminck@ugent.be>
2025-12-25 14:59:11 +0100

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -80,6 +80,24 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      {/* Hotjar Tracking */}
+      <Script
+        id="hotjar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:HOTJAR_ID_HIER,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
+
       {/* Tawk.to live chat */}
       <Script
         id="tawk-to"
@@ -106,4 +124,5 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
 
